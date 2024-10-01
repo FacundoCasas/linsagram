4e7d70d47236ddcb985dc7ff4931a37041782994
Facundo Casas <facundo.casas.98@gmail.com>
2024-10-01 20:05:17 -0300

    first commit

---
+++ linsagram/src/main/java/com/faders/linsagram/LinsagramApplication.java
@@ -0,0 +1,14 @@
+package com.faders.linsagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinsagramApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		//SpringApplication.run(LinsagramApplication.class, args);
+	}
+
+}
---
+++ linsagram/src/test/java/com/faders/linsagram/LinsagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faders.linsagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinsagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
